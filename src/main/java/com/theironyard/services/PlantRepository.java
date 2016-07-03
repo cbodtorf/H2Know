18d04fd497e858c0bc672bb581219c04c6b00926
jonathandavidblack <jonnybl915@gmail.com>
2016-07-03 18:30:54 -0400

    added "/manager/userPlantList" GET route added "/manager" DELETE route added "/manager/userPlantList" POST route

--- src/main/java/com/theironyard/services/PlantRepository.java
+++ src/main/java/com/theironyard/services/PlantRepository.java
@@ -1,6 +1,7 @@
 package com.theironyard.services;
 
 import com.theironyard.entities.Plant;
+import com.theironyard.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
  * Created by jonathandavidblack on 7/1/16.
  */
 public interface PlantRepository extends CrudRepository<Plant, Integer> {
+    public Iterable<Plant> findByUser(User user);
+    public Plant findOneByGardener(User gardener);
 }
